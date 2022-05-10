38a110f23dce3db071c8cfe8c9c462391a2a8b9d
mrdraxs <guilhermedoctormaster@gmail.com>
2022-05-10 10:59:58 -0300

    php

---
+++ advanced/banco.php
@@ -0,0 +1,28 @@
+<?php
+
+function exibeMensagem($mensagem)
+{
+	echo $mensagem . PHP_EOL;
+}
+
+$contasCorrentes = [
+    '123.456.789-10' => [
+        'titular' => 'Maria',
+        'saldo' => 10000
+    ],
+    '123.456.789-11' => [
+        'titular' => 'Alberto',
+        'saldo' => 300
+    ],
+    '123.256.789-12' => [
+        'titular' => 'Vinicius',
+        'saldo' => 100
+    ]
+];
+
+$contasCorrentes['123.456.789-10']['saldo'] -= 500;
+
+foreach ($contasCorrentes as $cpf => $conta)
+{ 
+	exibeMensagem($cpf . "" . $conta ['titular'] . ' ' . $conta['saldo']);
+}
